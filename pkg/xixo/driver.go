9301525ffc253cb45bdbe5fe475948d6855beb56
Youen Péron <youen.peron@cgi.com>
2023-09-22 15:56:03 +0000

    docs: driver comments

--- pkg/xixo/driver.go
+++ pkg/xixo/driver.go
@@ -4,51 +4,67 @@ import (
 	"io"
 )
 
+// ShellDriver represents a driver that processes XML using shell commands.
 type ShellDriver struct {
 	parser    *XMLParser
 	processes []*Process
 }
 
+// NewShellDriver creates a new ShellDriver instance with the given reader, writer, and callbacks.
 func NewShellDriver(reader io.Reader, writer io.Writer, callbacks map[string]string) ShellDriver {
+	// Create a new XML parser with XPath enabled and initialize processes.
 	parser := NewXMLParser(reader, writer).EnableXpath()
 	processes := []*Process{}
 
+	// Iterate through the callbacks and create a process for each element.
 	for elementName, shell := range callbacks {
 		process := NewProcess(shell)
+		// Register a JSON callback for the element and add the process to the list.
 		parser.RegisterJSONCallback(elementName, process.Callback())
 		processes = append(processes, process)
 	}
 
+	// Return the ShellDriver with the parser and processes.
 	return ShellDriver{parser: parser, processes: processes}
 }
 
+// Stream processes the XML using the registered processes and returns any error encountered.
 func (d ShellDriver) Stream() error {
+	// Start each process in parallel.
 	for _, process := range d.processes {
 		if err := process.Start(); err != nil {
 			return err
 		}
 	}
 
+	// Stream the XML using the parser and return any error encountered.
 	err := d.parser.Stream()
 
 	return err
 }
 
+// FuncDriver represents a driver that processes XML using callback functions.
 type FuncDriver struct {
 	parser *XMLParser
 }
 
+// NewFuncDriver creates a new FuncDriver instance with the given reader, writer, and callbacks.
 func NewFuncDriver(reader io.Reader, writer io.Writer, callbacks map[string]CallbackMap) FuncDriver {
+	// Create a new XML parser with XPath enabled.
 	parser := NewXMLParser(reader, writer).EnableXpath()
 
+	// Register callback functions for each element.
 	for elementName, function := range callbacks {
 		parser.RegisterMapCallback(elementName, function)
 	}
 
+	// Return the FuncDriver with the parser.
 	return FuncDriver{parser: parser}
 }
 
+// Stream processes the XML using registered callback functions and returns any error encountered.
 func (d FuncDriver) Stream() error {
+	// Stream the XML using the parser and return any error encountered.
 	err := d.parser.Stream()
 
 	return err
--- pkg/xixo/driver_test.go
+++ pkg/xixo/driver_test.go
@@ -8,15 +8,18 @@ import (
 	"github.com/youen/xixo/pkg/xixo"
 )
 
-// TestMapCallback should convert.
+// TestDriverSimpleEdit tests the ShellDriver by creating a reader, writer, and a map of subscribers,
+// then it processes the XML and asserts the expected output.
 func TestDriverSimpleEdit(t *testing.T) {
 	t.Parallel()
 
+	// Create a reader with an XML string, an empty writer, and a map of subscribers.
 	reader := bytes.NewBufferString("<root><element1>innerTexta</element1></root>")
 	writer := bytes.Buffer{}
 	subscribers := map[string]string{"root": "tr a b"}
 	driver := xixo.NewShellDriver(reader, &writer, subscribers)
 
+	// Stream the XML using the driver and assert the expected output.
 	err := driver.Stream()
 	assert.Nil(t, err)
 
@@ -24,9 +27,12 @@ func TestDriverSimpleEdit(t *testing.T) {
 	assert.Equal(t, expected, writer.String())
 }
 
+// TestFuncDriverEdit tests the FuncDriver by creating a reader, writer, and a callback function,
+// then it processes the XML and asserts the expected output and that the callback was called.
 func TestFuncDriverEdit(t *testing.T) {
 	t.Parallel()
 
+	// Create a reader with an XML string, an empty writer, a callback function, and a driver.
 	reader := bytes.NewBufferString("<root><element1>innerTexta</element1></root>")
 	writer := bytes.Buffer{}
 	called := false
@@ -40,6 +46,7 @@ func TestFuncDriverEdit(t *testing.T) {
 	subscribers := map[string]xixo.CallbackMap{"root": callback}
 	driver := xixo.NewFuncDriver(reader, &writer, subscribers)
 
+	// Stream the XML using the driver, assert the expected output, and check if the callback was called.
 	err := driver.Stream()
 	assert.Nil(t, err)
 
